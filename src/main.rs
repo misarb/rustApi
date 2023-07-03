d7b5a3a7ecf2964d97c9a2c1188ccb4dbf6b8d56
lahcen BOULBALAH <lahcen.boulbalah@alten.com>
2023-07-03 12:46:15 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,48 @@
+use std::env;
 use std::error::Error;
 
-use ureq;
-use serde::{Serialize,Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json;
+use ureq;
 
-#[derive(Serialize , Deserialize,Debug)]
-struct Article{
-    title : String,
-    url : String,
+#[derive(Serialize, Deserialize, Debug)]
+struct Article {
+    title: String,
+    url: String,
 }
 
-#[derive(Serialize,Deserialize,Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct NewsApi {
-    status : String,
-    totalResults : u32,
-    articles : Vec<Article>,
-    
+    status: String,
+    totalResults: u32,
+    articles: Vec<Article>,
 }
 
-
-
-fn get_article(url:&str) -> Result<Vec<Article>,Box<dyn Error>>{
-    let resp =ureq::get(url).call()?.into_string()?;
+fn get_article(url: &str) -> Result<Vec<Article>, Box<dyn Error>> {
+    let resp = ureq::get(url).call()?.into_string()?;
     //Ok(resp)
     // conver the string respo to json
-     let articales:NewsApi = serde_json::from_str(&resp)?;    
-     Ok(articales.articles)
+    let articales: NewsApi = serde_json::from_str(&resp)?;
+    Ok(articales.articles)
 }
 
-fn render_article(articles:&Vec<Article>){
-    
+fn render_article(articles: &Vec<Article>) {
     for article in articles {
-    println!("title : {}",article.title);
-    println!("url : {}",article.url);
-    println!("---");
+        println!("title : {}", article.title);
+        println!("url : {}", article.url);
+        println!("---");
     }
-    
 }
 
-
-
-fn main()-> Result<(),Box<dyn Error>> {
-    let url ="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6b14d54a26444a22ba0be335d6fd4c6d";
+fn main() -> Result<(), Box<dyn Error>> {
+    let api_key = env::var("API_KEY").map_err(|_| "API_KEY environment variable not set")?;
+    let url = format!(
+        "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={}",
+        api_key
+    );
     let articles = get_article(&url)?;
     render_article(&articles);
 
-  
     // println!("{:#?}",article);
     // dbg!(article);
     Ok(())
--- target/debug/deps/articel_Api-f4582652c1ddf0da.d
+++ target/debug/deps/articel_Api-f4582652c1ddf0da.d
@@ -1,5 +1,5 @@
-/home/misar/Github/rustthings/articel_Api/target/debug/deps/articel_Api-f4582652c1ddf0da: src/main.rs
+/home/misar/Github/articel_Api/target/debug/deps/articel_Api-f4582652c1ddf0da: src/main.rs
 
-/home/misar/Github/rustthings/articel_Api/target/debug/deps/articel_Api-f4582652c1ddf0da.d: src/main.rs
+/home/misar/Github/articel_Api/target/debug/deps/articel_Api-f4582652c1ddf0da.d: src/main.rs
 
 src/main.rs:
